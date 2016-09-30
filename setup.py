154e5e90af3280aa94c31a5a70a16c826f1db01d
Danilo Bargen <mail@dbrgn.ch>
2016-09-30 16:16:25 +0200

    Add setup and changelog

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+readme = open('README.rst').read()
+
+setup(name='drf_dynamic_fields',
+      version='0.1.0',
+      description='Dynamically return subset of Django REST Framework serializer fields',
+      author='Danilo Bargen',
+      author_email='mail@dbrgn.ch',
+      url='https://github.com/dbrgn/drf-dynamic-fields',
+      packages=['drf_dynamic_fields'],
+      zip_safe=True,
+      include_package_data=True,
+      license='MIT',
+      keywords='drf restframework rest_framework django_rest_framework serializers',
+      long_description=readme,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+)
