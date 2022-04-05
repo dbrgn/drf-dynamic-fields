ed4e196050e37c31d865344d4eea26023553ae03
Danilo Bargen <mail@dbrgn.ch>
2022-04-05 22:32:35 +0200

    Format source with black
    The formatting is enforced through CI.

--- runtests.py
+++ runtests.py
@@ -15,14 +15,14 @@ from django.test.utils import get_runner
 
 def run_tests(*test_args):
     if not test_args:
-        test_args = ['tests']
+        test_args = ["tests"]
 
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+    os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
     django.setup()
     test_runner = get_runner(settings)()
     failures = test_runner.run_tests(test_args)
     sys.exit(bool(failures))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_tests(*sys.argv[1:])
